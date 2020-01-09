76711688d848045f45e12ce6586a823ce6fc2dd4
Mokhtar Ali <joemokhtarali@gmail.com>
2020-01-09 18:07:39 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School 
+  def initialize(name)
+    @name = name 
+    @roster = {}
+    @grade = grade
+  end 
+  
+  attr_accessor :name, :roster,
+  attr_reader 
+  
+  def roster
+     {}
+  end 
+  
+  def add_student(name, grade)
+    if hash[grade] 
+      hash[grade] << name
+    else 
+      hash[grade] = []
+      hash[grade] << name
+    end 
+    
+  end
+  
+  
+end  
